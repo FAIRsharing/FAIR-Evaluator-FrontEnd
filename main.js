62f16e9ef7c16ee60be7b41ad9e1ed6c6afb6649
Terazus <batistadominique@hotmail.com>
2019-03-20 04:44:00 -0700

    added create new collection (no form validation yet)

--- main.js
+++ main.js
@@ -73,6 +73,11 @@
             })
         };
 
+        $scope.goToNewCollection = function(){
+            $scope.baseURL = new $window.URL($location.absUrl());
+            $window.location.href = $scope.baseURL.origin + $scope.baseURL.pathname + '#!/collection/new';
+        }
+
     });
 
     /* *************************************************************************************************** */
@@ -153,7 +158,7 @@
     });
 
     /* route: /collections */
-    my_app.controller("newCollectionCtrl", function($http, $scope, $window, $location){
+    my_app.controller("newCollectionCtrl", function($http, $scope, $window, $location, $timeout){
 
         let request = {
             method: 'GET',
@@ -164,18 +169,70 @@
             data: null
         };
 
-        $scope.available_collections = {};
+        $scope.available_metrics = {};
         $scope.collection_data = {};
         $scope.collection_data.name = "";
         $scope.collection_data.contact = "";
         $scope.collection_data.organization = "";
         $scope.collection_data.indicators = [];
         $scope.collection_data.description = "";
+        $scope.triggered = false;
+        $scope.errors = false;
+        $scope.response_content = false;
 
 
         $http(request).then(function(response){
-            $scope.available_collections = response.data;
+            $scope.available_metrics = response.data;
         });
+
+        $scope.createCollection = function(coll){
+
+            $scope.triggered = true;
+
+            let request_data = {
+                "name": $scope.collection_data.name,
+                "contact": $scope.collection_data.contact,
+                "description": $scope.collection_data.description,
+                "organization": $scope.collection_data.organization,
+                "include_metrics": []
+            };
+
+            for (let ite in $scope.collection_data.indicators){
+                let metric_URL = $scope.collection_data.indicators[ite];
+                for (let sub_it in $scope.available_metrics){
+                    if ($scope.available_metrics[sub_it]["@id"] === metric_URL){
+                        request_data['include_metrics'].push($scope.available_metrics[sub_it]['smarturl']);
+                    }
+                }
+            }
+
+            let request = {
+                method: 'POST',
+                url: base_url + "/collections",
+                headers: {
+                    'Accept': "application/json",
+                    'Content-Type': "application/json"
+                },
+                data: request_data
+            };
+
+            $http(request).then(function(response){
+                $scope.triggered = false;
+                console.log(response);
+                let identifier= response.data['@id'].split('/').slice(-1)[0];
+                let root_url = new $window.URL($location.absUrl());
+                $scope.response_content = root_url.origin + root_url.pathname + "#!/collections/" + identifier;
+                $scope.errors = response.data.statusText
+            });
+        };
+
+        $scope.clearFields = function(){
+            $scope.collection_data.name = "";
+            $scope.collection_data.contact = "";
+            $scope.collection_data.organization = "";
+            $scope.collection_data.indicators = [];
+            $scope.collection_data.description = "";
+        }
 
     });
 
